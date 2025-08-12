67601c048d71eba20541e125ebb884f88b66d7f8
fensosystems <pascal.fomeh@fensosystems.com>
2025-08-12 16:22:01 +0000

    added terraform and provider section in providers.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-tf-zyq-state-file-bucket"
+    key    = "production/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "tf_locking_table"
+    encrypt        = true
+  }
+}
 
+provider "aws" {
+  # Configuration options for the AWS provider (e.g., region)
+  region = "us-east-1"
+}
