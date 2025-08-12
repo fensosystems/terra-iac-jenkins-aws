94f75d88bae239393a4ce656d5b2063371c5ceae
fensosystems <pascal.fomeh@fensosystems.com>
2025-08-12 18:35:59 +0000

    bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
-
+    resource "aws_s3_bucket" "my_s3_bucket" {
+        bucket = "my-terraform-jenkins-test-buck" # Replace with a globally unique name
+        acl    = "private" # Or "public-read", "public-read-write", etc.
+        tags = {
+            Environment = "Development"
+            Project     = "TerraformS3"
+        }
+    }
