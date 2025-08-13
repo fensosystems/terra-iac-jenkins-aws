1bddfeda9068e5ae4e82498e16a554608c374f09
fensosystems <pascal.fomeh@fensosystems.com>
2025-08-13 03:11:28 +0000

    added resource eip

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@
             Project     = "TerraformS3"
         }
     }
+
+    resource "aws_eip" "example_eip" {
+    vpc = true # Set to true if the EIP is for use within a VPC
+    # Optional: Associate with an instance or network interface
+    # instance = aws_instance.web.id
+    # network_interface = aws_network_interface.main.id
+    }
